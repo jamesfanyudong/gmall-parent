32a8b7ba73433845283afb98c2d9dc114034bd7d
fanyudong <3222586909@qq.com>
2022-06-21 13:50:40 +0800

    完成属性信息查询

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/CategoryController.java
@@ -3,9 +3,14 @@ package com.atguigu.gmall.product.controller;
 
 import com.atguigu.gmall.common.result.Result;
 import com.atguigu.gmall.model.product.BaseCategory1;
+import com.atguigu.gmall.model.product.BaseCategory2;
+import com.atguigu.gmall.model.product.BaseCategory3;
 import com.atguigu.gmall.product.service.BaseCategory1Service;
+import com.atguigu.gmall.product.service.BaseCategory2Service;
+import com.atguigu.gmall.product.service.BaseCategory3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,18 +23,49 @@ public class CategoryController {
 
     @Autowired
     BaseCategory1Service baseCategory1Service;
+
+    @Autowired
+    BaseCategory3Service baseCategory3Service;
     /**
      *
      * 查询一级分类
      * @return
      */
-    @GetMapping
+    @GetMapping("/getCategory1")
     public Result getCategory1(){
         List<BaseCategory1> category1s = baseCategory1Service.list();
         return Result.ok(category1s);
 
     }
 
+    // admin/product/getCategory2/4
 
+    /**
+     * 获取二级分类列表
+     * @param category1Id
+     * @return
+     */
+
+    @GetMapping("/getCategory2/{category1Id}")
+    public Result getCategory2(@PathVariable Long category1Id){
+        List<BaseCategory2> category2s = baseCategory1Service.getCategory2(category1Id);
+        return Result.ok(category2s);
+
+    }
+
+    //product/getCategory3/17
+
+    /**
+     * 根据二级分类id查询三级分类列表
+     * @param category2Id
+     * @return
+     */
+
+    @GetMapping("/getCategory3/{category2Id}")
+    public Result getCategory3(@PathVariable Long category2Id){
+        List<BaseCategory3> category3s = baseCategory3Service.getCategory3(category2Id);
+        return Result.ok(category3s);
+
+    }
 
 }
---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/BaseCategory2ServiceImpl.java
@@ -0,0 +1,26 @@
+package com.atguigu.gmall.product.service.impl;
+
+import com.atguigu.gmall.model.product.BaseCategory2;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import com.atguigu.gmall.product.service.BaseCategory2Service;
+import com.atguigu.gmall.product.mapper.BaseCategory2Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ *
+ */
+@Service
+public class BaseCategory2ServiceImpl extends ServiceImpl<BaseCategory2Mapper, BaseCategory2>
+    implements BaseCategory2Service{
+
+
+
+}
+
+
+
+
